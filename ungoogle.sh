bdcdefdd495fcf70a764202c00e433d980b10a94
Marcos Marado <mindboosternoori@gmail.com>
2017-02-27 23:59:36 +0000

    Check if argument exists

--- ungoogle.sh
+++ ungoogle.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]
+	then echo "No arguments supplied"
+	exit
+fi
+
 encoded=$(cat $1|sed 's/url=/#/'|cut -d# -f2)
 printf -v decoded '%b' "${encoded//%/\\x}"
 echo "${decoded}"
