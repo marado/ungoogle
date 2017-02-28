d933958b531db0f6cc37ac0d29016aea3b2a81d0
Marcos Marado <mindboosternoori@gmail.com>
2017-02-28 00:14:31 +0000

    Check if filename provided in argument exists

--- ungoogle.sh
+++ ungoogle.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]
-	then echo "No arguments supplied"
+	then echo "No arguments supplied. Please provide a file name with a Google URL in it."
+	exit
+fi
+
+if [ ! -e "$1" ]
+	then echo "No file named $1 exists."
 	exit
 fi
 
